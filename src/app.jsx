558873e72725cdf37ade05ad24ffaa4dd0d814da
Danny <seunghoonlee95@gmail.com>
2021-08-03 22:05:11 +0900

    show video list with title

--- src/app.jsx
+++ src/app.jsx
@@ -1,10 +1,24 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import './app.css';
+import VideoList from './components/video_list/video_list';
 
 function App() {
-  return (
-    <h1>Hello :)</h1>
-  );
+  const [videos, setVideos] = useState([]);
+
+  useEffect(()=>{
+    const requestOptions = {  //아래 두줄은 우리가 fetch를 쓸 때, request를 할 때 옵션을 전달하는 것.
+      method: 'GET',
+      redirect: 'follow'
+    };
+    
+    fetch("https://youtube.googleapis.com/youtube/v3/videos?part=snippet&chart=mostPopular&maxResults=25&key=AIzaSyBx7zpz1TGmzf1SUo5r1hzpA_zbnESv9vw", requestOptions)
+      .then(response => response.json())
+      .then(result => setVideos(result.items))
+      .catch(error => console.log('error', error));
+  }, []);
+
+  return <VideoList videos={videos}/>;
+
 }
 
 export default App;
---
+++ src/components/video_item/video_item.jsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const VideItem = (props) => (
+  <h1>{props.video.snippet.title}</h1>
+  );
+
+export default VideItem;
