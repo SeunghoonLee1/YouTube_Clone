69aebfad8dcf5aae1d747ad6a5eb2418491e21a8
Danny <seunghoonlee95@gmail.com>
2021-08-04 15:30:21 +0900

    make video item

--- src/app.jsx
+++ src/app.jsx
@@ -5,7 +5,9 @@ import VideoList from './components/video_list/video_list';
 function App() {
   const [videos, setVideos] = useState([]);
 
+  //Component가 mount & update되었을 때 호출함.
   useEffect(()=>{
+    //아래의 code block은 postman을 통해 받아온 youtube API임!
     const requestOptions = {  //아래 두줄은 우리가 fetch를 쓸 때, request를 할 때 옵션을 전달하는 것.
       method: 'GET',
       redirect: 'follow'
