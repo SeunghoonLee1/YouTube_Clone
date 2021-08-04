6e39b8b3dcea9ec6f0be559049b4d153888ee9c9
Danny <seunghoonlee95@gmail.com>
2021-08-04 23:14:58 +0900

    implemented search function

--- src/app.jsx
+++ src/app.jsx
@@ -5,7 +5,23 @@ import SearchHeader from './components/search_header/search_header';
 
 function App() {
   const [videos, setVideos] = useState([]);
-
+  const search = (query) => {
+    const requestOptions = {
+      method: 'GET',
+      redirect: 'follow'
+    };
+    
+    fetch(`https://youtube.googleapis.com/youtube/v3/search?part=snippet&maxResults=25&q=${query}&type=video&key=AIzaSyBx7zpz1TGmzf1SUo5r1hzpA_zbnESv9vw`, requestOptions)
+      .then(response => response.json())
+      .then(result => result.items.map(item => ({...item, id: item.id.videoId}))) 
+      //items배열을 map을 통해서 돌면서 배열안에 있는 각각의 아이템을 새로운 오브젝트! 로 변환해줌. ({}을 썼으므로..)
+      //이때 새로운 objectsms 기존 아이템의 동일한 정보를 갖고 있으나 id만 새로운 값으로 덮어 씌워진다!
+      //Q. 이과정을 왜하는가? 
+      //A. 아래의 videoList를 받아오는 API에선 items.id에 하나의 문자열로 이루어진 id가 있었지만, 이번에는 items.id가 
+      //  객체로 존재하므로 오류가 발생.. 하나의 문자열로 이루어진 Id를 넣어주기 위해서 이 과정을 진행!
+      .then(items => setVideos(items))
+      .catch(error => console.log('error', error));
+  };
   //Component가 mount & update되었을 때 호출함.
   useEffect(()=>{
     //아래의 code block은 postman을 통해 받아온 youtube API임!
@@ -22,7 +38,7 @@ function App() {
 
   return (
     <idiv className={styles.app}>
-      <SearchHeader />
+      <SearchHeader onSearch={search}/>
       <VideoList videos={videos}/>
     </idiv>
   );
--- src/components/search_header/search_header.jsx
+++ src/components/search_header/search_header.jsx
@@ -1,14 +1,31 @@
-import React from 'react';
+import React, {useRef} from 'react';
 import styles from './search_header.module.css';
 
 const SearchHeader = ((props) => {
-  const inputRef = React.createRef();
+  const inputRef = React.useRef();
   
   const onSubmit = (event) => {
     event.preventDefault();
     const searchWord = inputRef.current.value;
-
   };
+
+  const handleSearch = ()=>{
+    const value = inputRef.current.value;
+    props.onSearch(value);
+    //만약 위에서 const SearchHandler = (({onSearch})) => ...였다면
+    //여기에서도 그냥 onSearch(value);라고 부름!!
+  };
+
+  const onClick = (event) =>{
+    handleSearch();
+  };
+
+  const onKeyPress = (event) =>{
+    if(event.key === 'Enter'){
+      handleSearch();
+    }
+  }
+  
 
   return(
     <header className={styles.header}>
@@ -20,9 +37,10 @@ const SearchHeader = ((props) => {
         type="search"
         className={styles.searchBox}
         placeholder="Search.."
+        onKeyPress={onKeyPress}
       />
       {/* form에서는 button이 눌리면 submit이라는 event가 발생한다! */}
-      <button type="submit" className={styles.searchBtn}>
+      <button type="submit" className={styles.searchBtn} onClick={onClick}>
         <img 
         className={styles.buttonImg}
         src="/images/search.png" 
